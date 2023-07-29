c42a1ac911f75f9b109286c3a12789a40f1178b0
Jonathan Raine <jonatannnraineeee@gmail.com>
2023-07-29 16:18:23 +0700

    routing 2
    routing 2

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,30 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
 function Home(){
+
+    const router = useRouter()
+
+    const handleClick = () => {
+        console.log("placing your order")
+        router.replace('/product')
+    }
+
     return(
-        <h1>hello</h1>
+        <div>
+            <h1>homepage</h1>
+            <Link href="/blog">
+                {/* gsb taro tag a */}
+                {/* <a id="link">blog</a> */}
+                <p>blog</p>
+            </Link>
+            <Link href="/product">
+                <p>product</p>
+            </Link>
+            <button onClick={handleClick}>
+                Place order
+            </button>
+        </div>
     )
 }
 
--- pages/product/index.jsx
+++ pages/product/index.jsx
@@ -1,9 +1,23 @@
-function Product(){
+import Link from 'next/link'
+
+function Product({ ProductID = 100}){
     return(
         <>
-            <h2>prod 1</h2>
-            <h2>prod 2</h2>
-            <h2>prod 3</h2>
+            <Link href="/">
+                <p>Home</p>
+            </Link>
+            <Link href="/product/1">
+                <h2>prod 1</h2>
+            </Link>
+            <Link href="/product/2">
+                <h2>prod 2</h2>
+            </Link>
+            <Link href="/product/3" replace>
+                <h2>prod 3</h2>
+            </Link>
+            <Link href={`/product/${ProductID}`}>
+                <h2>prod {ProductID}</h2>
+            </Link>
         </>
     )
 }
